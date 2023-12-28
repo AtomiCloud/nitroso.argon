ecd871ecec8c408856995b0935f8669e53df800e
kirinnee <kirinnee97@gmail.com>
2023-12-28 10:34:25 +0800

    feat: change initial landing page design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,35 @@
 <script lang="ts">
     import {config} from "../config/shared";
     import Page from "$lib/components/complex/page.svelte";
+    import mascot from "$lib/assets/nitroso-mascot.png";
+    import {Button} from "$lib/components/ui/button";
+    import {BookOpenCheck} from "lucide-svelte";
     console.log("Configuration", config.app);
 
 
 </script>
+
+<style lang="css">
+
+</style>
+
 <Page notFoundMessage="Main page cannot be found">
-
-    <div class="max-w-[1200px] w-11/12 mx-auto">
-        <div class="relative text-center text-5xl md:text-8xl  flex flex-col justify-center ">
-            <div class="w-full h-full absolute font-semibold
-            transition-all duration-500
-        bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))]
-        bg-opacity-[50%]
-        from-orange-300 from-0% via-background via-60% to-background to-90% blur opacity-[25%] dark:opacity-[15%]">
-
+    <div class="flex flex-col gap-4 justify-center align-center w-11/12 max-w-[1200px] mx-auto">
+        <div class="flex flex-row-reverse flex-wrap justify-center align-center">
+            <div>
+                <img src={mascot} alt="BunnyBooker" class="h-128 w-128 md:h-64 md:w-64"/>
             </div>
-            <div class="flex flex-col gap-4 items-center mt-4 md:mt-28 mb-4 w-full font-semibold">
-                <div class="mx-auto relative p-2 md:p-4  dark:bg-gradient-to-br bg-gradient-to-tr from-red-700 via-orange-400 to-yellow-100 inline-block text-transparent bg-clip-text">BunnyBooker</div>
-                <div class="text-2xl md:text-4xl font-light">
-                    Stress-free ticket booking
+            <div class="flex flex-col gap-4 justify-evenly align-center">
+                <div class="text-5xl md:text-7xl text-center md:text-left ">
+                    <span>Bunny</span><span>Booker</span>
                 </div>
+                <div class="text-lg md:text-xl text-center md:text-left ">
+                    <span>Your stress-free ticket booking assistant</span>
+                </div>
+                <Button>
+                    <BookOpenCheck class="mr-2 h-4 w-4"/>
+                    Start Booking
+                </Button>
             </div>
         </div>
 
